79209dc0457c9778f4fb1f517987b04739132078
Gerogiannis <54914456+GEROGIANNIS@users.noreply.github.com>
2024-05-31 01:27:05 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,12 +8,12 @@ struct Resolution { // struct to store width and height of a resolution
     int height;
 };
 
-vector<Resolution> findResolutions(int totalPixels) { // function to find resolutions with 4:3 aspect ratio and total pixels
+vector<Resolution> findResolutions(int totalPixels, int aspectWidth, int aspectHeight) { // function to find resolutions with specified aspect ratio and total pixels
     vector<Resolution> resolutions;
     for (int width = 1; width <= totalPixels; ++width) { // iterate through all possible widths
         if (totalPixels % width == 0) { // if total pixels is divisible by width
             int height = totalPixels / width;   // calculate height
-            if ((width * 3) == (height * 4)) {  // check if aspect ratio is 4:3
+            if ((width * aspectHeight) == (height * aspectWidth)) {  // check if aspect ratio matches
                 Resolution res = {width, height};
                 resolutions.push_back(res);
             }
@@ -24,13 +24,23 @@ vector<Resolution> findResolutions(int totalPixels) { // function to find resolu
 
 int main() { // main function
     int totalPixels = 30000; // total pixels of a resolution
-    vector<Resolution> resolutions = findResolutions(totalPixels); // find resolutions with 4:3 aspect ratio and total pixels
 
-    if (resolutions.empty()) { // if no resolutions found
+    cout << "Finding resolutions with 4:3 aspect ratio:\n";
+    vector<Resolution> resolutions_4_3 = findResolutions(totalPixels, 4, 3); // find resolutions with 4:3 aspect ratio and total pixels
+    if (resolutions_4_3.empty()) { // if no resolutions found
         cout << "No resolutions found with 4:3 aspect ratio and " << totalPixels << " pixels.\n";
     } else { // if resolutions found
-        cout << "Resolutions with 4:3 aspect ratio and " << totalPixels << " pixels:\n"; 
-        for (const auto& res : resolutions) { // print resolutions
+        for (const auto& res : resolutions_4_3) { // print resolutions
+            cout << res.width << "x" << res.height << endl; 
+        }
+    }
+
+    cout << "\nFinding resolutions with 16:9 aspect ratio:\n";
+    vector<Resolution> resolutions_16_9 = findResolutions(totalPixels, 16, 9); // find resolutions with 16:9 aspect ratio and total pixels
+    if (resolutions_16_9.empty()) { // if no resolutions found
+        cout << "No resolutions found with 16:9 aspect ratio and " << totalPixels << " pixels.\n";
+    } else { // if resolutions found
+        for (const auto& res : resolutions_16_9) { // print resolutions
             cout << res.width << "x" << res.height << endl; 
         }
     }
